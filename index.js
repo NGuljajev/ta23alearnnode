24a4361729209b6a30cf9c0e6c5087f29f9e9309
Nikita Guljajev <Nikita Guljajev>
2024-10-21 13:25:05 +0300

    Add boxen

--- index.js
+++
@@ -1,8 +0,0 @@
-const common = require('./common.js');
-common.hello()
-const es = require('./es.js').default;
-es.hello();
-
-//const fs = require('fs'); //commonjs
-//import fs from 'fs'; // es module
-//fs.writeFileSync('test.txt', 'hello node');
