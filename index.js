e700f30b0559ab2de5437f8c0e2785c09772e724
Nikita Guljajev <Nikita Guljajev>
2024-10-21 12:52:18 +0300

    es vs commonjs

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const fs = require('fs');
-fs.writeFileSync('test.txt', 'hello node');
+const common = require('./common.js');
+common.hello();
+import es from './es.js';
+es.hello();
+
+//const fs = require('fs'); //commonjs
+//import fs from 'fs'; // es module
+//fs.writeFileSync('test.txt', 'hello node');
