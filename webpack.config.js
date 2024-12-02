b2a7cf93a512cb7d5a228b68350d2fcd5ef72f1a
Nikita Guljajev <Nikita Guljajev>
2024-12-02 13:44:23 +0200

    changes12.2.24

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,17 @@ export default async () => {
 let response = await fetch('https://rickandmortyapi.com/api/character');
 let json = await response.json();
 let characters = json.results;
-console.log(json);
+let pages = [];
+characters.forEach(character => {
+  let page = new HtmlWebpackPlugin({
+    template: './src/character.njk',
+    filename: 'character_' + character.id + '.html', 
+    templateParameters: {
+      character
+    },
+  });
+  pages.push(page);
+});
 
   return {
   entry: './src/index.js',
@@ -65,7 +75,8 @@ console.log(json);
     new HtmlWebpackPlugin({
       filename: 'about.html',
       template: './src/about.njk',
-    })
+    }),
+    ...pages
   ],
 };
 };
