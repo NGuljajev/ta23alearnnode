cd8058f8e3995be10b99cbcb00bd2c890ab909ee
NGuljajev <nguljajev@gmail.com>
2025-01-14 11:37:07 +0200

    add itemlist example

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <script setup>
 import { ref } from 'vue';
 
-let message = ref('Hello ta23a')
+let message = ref('')
+let items = ref(['Piim', 'Viin', 'Kvas', 'Noodles'])
 
-setTimeout(()=>{
-    message.value = 'Goodbye ta23a';
-},5000);
-
-
+function addItem(){
+    if(message.value.trim() !== ''){
+        items.value.push(message.value.trim());
+    }
+    message.value = '';
+}
 
 </script>
 
 <template>
-    <div class="container">
-    <h1>{{ message.split('').reverse().join('') }}</h1>
-    <button class="button is-primary is-dark" @click="message='We are back'">Click</button>
-    <input class="input" v-model="message">
-</div>
+    <div class="container mt-2">
+        <div class="field has-addons">
+            <div class="control">
+                <input class="input" type="text" v-model="message" @keypress.enter="addItem">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click=addItem>
+                    Add Item
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <h3>All Items</h3>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <style></style>
