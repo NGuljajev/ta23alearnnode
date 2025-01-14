59fc7db539f4e4a79789be2a8246a9f177e59756
NGuljajev <nguljajev@gmail.com>
2025-01-14 11:11:16 +0200

    1.14.2025

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup>
-let message = 'Hello ta23a'
+import { ref } from 'vue';
+
+let message = ref('Hello ta23a')
+
+setTimeout(()=>{
+    message.value = 'Goodbye ta23a';
+},5000);
+
+
+
 </script>
 
 <template>
-    <h1>{{ message }}</h1>
+    <div class="container">
+    <h1>{{ message.split('').reverse().join('') }}</h1>
+    <button class="button is-primary is-dark" @click="message='We are back'">Click</button>
+    <input class="input" v-model="message">
+</div>
 </template>
 
-<style>
-</style>
+<style></style>
