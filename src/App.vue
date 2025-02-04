2410fd4f39359005a5cdf76eabcbd1ac635ef0c5
NGuljajev <nguljajev@gmail.com>
2025-02-04 11:19:00 +0200

    add modal components and examples

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <script setup>
 import { ref } from 'vue';
+import Modal from './components/Modal.vue';
+import ImgModal from './components/ImgModal.vue';
 
 
 let modalActive = ref(false);
+let modalActive2 = ref(false);
+let modalActive3 = ref(false);
 
 
-document.body.addEventListener('keypress', event => {
-    if(event.key === 'Escape'){
-        modalActive.value = false;
-    }
-});
+
 </script>
 
 <template>
     <div class="container">
         <section class="section">
+            <div class="buttons">
             <button class="button is-primary" @click ="modalActive=true">Modal Activate</button>
+            <button class="button is-warning is-outline ml-2" @click ="modalActive2=true">Modal Activate</button>
+            <button class="button is-danger is-rounded ml-2" @click ="modalActive3=true">Modal Activate</button>
+        </div>
         </section>
     </div>
-    <div class="modal" :class="{ 'is-active': modalActive }">
-        <div class="modal-background"></div>
-        <div class="modal-content">
-            <p class="image is-4by3">
-                <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
-            </p>
-        </div>
-        <button class="modal-close is-large" @click ="modalActive=false" aria-label="close"></button>
-    </div>
+
+    <ImgModal :active="modalActive" @close="modalActive=false" url="https://picsum.photos/seed/nikita/200/300" html="<h1>Hello worlds</h1>"></ImgModal>
+
+    <ImgModal :active="modalActive2" @close="modalActive2=false" url="https://picsum.photos/seed/guljajev/200/300"></ImgModal>
+
+    <Modal :active="modalActive3" @close="modalActive3=false">
+        <div class="notification is-primary">
+  <button class="delete"></button>
+  Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum
+  dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta
+  nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus
+  diam, et dictum <a>felis venenatis</a> efficitur.
+</div>
+    </Modal>
 
 </template>
 
