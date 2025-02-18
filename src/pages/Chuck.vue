28f5cb0dcd056e4f26f53a3f0aac3459dffa2d57
NGuljajev <nguljajev@gmail.com>
2025-02-18 10:55:24 +0200

    Add fetch example

--- src/pages/Chuck.vue
+++ src/pages/Chuck.vue
@@ -4,21 +4,14 @@ import { ref } from 'vue';
 
 let joke = ref('');
 
-function reqListener() {
-    let data = JSON.parse(this.responseText);
-    console.log(data.value);
+fetch('https://api.chucknorris.io/jokes/random').then(response => {
+    return response.json();
+}).then(data => {
+    console.log(data);
     joke.value = data.value;
-}
-
-const req = new XMLHttpRequest();
-req.addEventListener("load", reqListener);
-req.open("GET", "https://api.chucknorris.io/jokes/random");
-req.send();
-
-
+});
 </script>
 
 <template>
-
  {{ joke }}
 </template>
