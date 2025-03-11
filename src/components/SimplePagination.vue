664f4a562613a35eb44eaa9bf1db406a918c28fc
NGuljajev <nguljajev@gmail.com>
2025-03-11 11:36:42 +0200

    added simplePagination  and pagination

---
+++ src/components/SimplePagination.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+    defineProps(['info', 'next', 'prev']);
+</script>
+<template>
+
+    <div class="buttons">
+        <button class="button is-primary" @click="$emit('prev')" :disabled="!info.prev">Prev</button>
+        <button class="button is-primary" @click="$emit('next')" :disabled="!info.next">Next</button>
+    </div>
+
+</template>
--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -2,11 +2,12 @@
 import axios from 'axios';
 import { ref } from 'vue';
 import CharacterCard from '../components/CharacterCard.vue';
+import Pagination from '../components/Pagination.vue';
 
 const characters = ref([]);
 const info = ref({});
-
-await getCharacters('https://rickandmortyapi.com/api/character?page=42');
+const currentPage = ref(2);
+await getCharacters('https://rickandmortyapi.com/api/character?page=1');
 
 
 async function getCharacters(url){
@@ -17,20 +18,19 @@ async function getCharacters(url){
 }
 
 async function next(){
+    currentPage.value++;
     await getCharacters(info.value.next);
 }
 
 async function prev(){
+    currentPage.value--;
     await getCharacters(info.value.prev);
 }
 
 </script>
 
 <template>
-    <div class="buttons">
-        <button class="button is-primary" @click="prev" :disabled="!info.prev">Prev</button>
-        <button class="button is-primary" @click="next" :disabled="!info.next">Next</button>
-    </div>
+    <Pagination :info="info" :current="currentPage" @next="next" @prev="prev"></Pagination>
     <div class="columns is-multiline">
         <div v-for="character in characters" class="column is-one-quarter">
             <CharacterCard :character="character"></CharacterCard>
